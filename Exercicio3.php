7eb67ddb2f6e668cdf98e2aec02b365a3ec9c90c
Caroline P. da Silva <86432393+carolinepedasil@users.noreply.github.com>
2021-09-20 13:40:54 -0300

    Create Exercicio3.php

---
+++ Exercicio3.php
@@ -0,0 +1,16 @@
+<?php
+
+//3) Crie um programa que leia uma idade de uma pessoa e calcule quantos dias essa pessoa já viveu.
+
+    echo "Digite sua data de nascimento, assim: AAAA-MM-DD (Ano/Mês/Dia) ";
+    $data = fgets(STDIN);
+    
+    $diff = (date('Y') - date('Y',strtotime($data)));
+
+    echo "Você tem $diff anos. ";
+
+    $diferenca = ($diff)*(365); //para este exercício, eu considerei que todos os anos tem exatamente 365 dias.
+
+    echo "Você já viveu $diferenca dias. ";
+
+?>
