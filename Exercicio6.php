823dfd37da7ba95bd2325257dbcbc442260f8f70
Caroline P. da Silva <86432393+carolinepedasil@users.noreply.github.com>
2021-09-26 13:01:40 -0300

    Create Exercicio6.php

---
+++ Exercicio6.php
@@ -0,0 +1,11 @@
+<?php
+
+//6) Crie um programa que lê um número real em dólar, e converte o valor para reais. Considere que a cotação é US$ 1 = R$ 5,47. Utilize uma constante para armazenar o valor do dolar.
+
+    echo "Digite quantos doláres você quer: ";
+    $dolar = fgets(STDIN);
+    $real = 5.47 * $dolar;
+
+    echo "US$$dolar vai dar R$$real."
+
+?>
