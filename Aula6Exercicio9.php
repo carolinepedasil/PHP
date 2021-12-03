16244f8263ccfec9c7bd273fc96620ed50388f34
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-12-03 17:52:11 -0300

    Create Aula6Exercicio9.php

---
+++ Aula6Exercicio9.php
@@ -0,0 +1,13 @@
+<?php
+
+// Crie uma função em linguagem PHP chamado Dado() que retorna, através de sorteio, um número de 1 até 6.
+
+function dado(){
+    $numero = rand(1, 6);
+    return $numero;
+}
+
+$dado = dado();
+echo "O número retornado foi $dado.";
+
+?>
